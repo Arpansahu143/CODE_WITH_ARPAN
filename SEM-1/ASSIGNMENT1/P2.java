8f9932de48cb9128d108ee439bea55ae42b09381
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:53:48 +0530

    Create P2.java

---
+++ SEM-1/ASSIGNMENT1/P2.java
@@ -0,0 +1,15 @@
+
+public class P2
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("*******       ******* ");
+		System.out.println("      *       *     *");
+		System.out.println("      *********     *******");
+		
+
+	}
+
+}
+
