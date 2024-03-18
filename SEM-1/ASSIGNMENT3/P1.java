77952b21e25784293b41258e2ad1b684bd2d46c1
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:54:58 +0530

    Create P1.java

---
+++ SEM-1/ASSIGNMENT3/P1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+public class P1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        if (age >= 18)
+            System.out.println("You are eligible to cast your vote.");
+
+        sc.close();
+    }
+}
