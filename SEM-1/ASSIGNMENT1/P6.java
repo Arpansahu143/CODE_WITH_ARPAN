0f43915cdad53f3718ca7add5066ba6e2df93c45
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:53:58 +0530

    Create P6.java

---
+++ SEM-1/ASSIGNMENT1/P6.java
@@ -0,0 +1,12 @@
+
+public class P6 {
+
+	public static void main(String[] args){
+		String name=" Arpan sahu";
+		int acc=  123456;
+		double bal=  764785.9;
+		System.out.println("\"my name is" +name + " bearing account number" +acc + " having balance"+bal +".\"");
+		
+	}
+
+}
