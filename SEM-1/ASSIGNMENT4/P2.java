d9c301c818356c64fe620dcd63ebefdb163e85fd
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:55:49 +0530

    Create P2.java

---
+++ SEM-1/ASSIGNMENT4/P2.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class P2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number: ");
+        int number = sc.nextInt();
+        int n = number, sum = 0;
+
+        while (n >= 1) {
+            sum += n % 10;
+            n /= 10;
+        }
+
+        if (sum % 9 == 0) {
+            System.out.println("The number " + number + " is divisible by 9.");
+        } else {
+            System.out.println("The number " + number + " is not divisible by 9.");
+        }
+
+        sc.close();
+    }
+}
