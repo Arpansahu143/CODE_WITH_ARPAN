22008cc968e68c94a5fa76ea3a83b8baa17dcf80
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:53:42 +0530

    Create homeassignment.java

---
+++ SEM-1/ASSIGNMENT1/homeassignment.java
@@ -0,0 +1,23 @@
+
+public class homeassignment {
+
+	public static void main(String[] args) {
+		System.out.println("QUESTION-1");
+		System.out.println(2+"bc");
+		System.out.println(2+3+"bc");
+		System.out.println((2+3)+"bc");
+		System.out.println("bc"+(2+3));
+		System.out.println("bc"+2+3);
+		System.out.println("       "
+				+ ""
+				+ ""
+				+ "");
+		
+		System.out.println("QUESTION-2");
+		System.out.println("b");
+		System.out.println('b'+'c');
+		System.out.println((char)+('a'+4));
+
+	}
+
+}
