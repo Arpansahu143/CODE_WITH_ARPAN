5a01ebef8be990863c9de41de11b676ecac7776b
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:54:00 +0530

    Create p7.java

---
+++ SEM-1/ASSIGNMENT1/p7.java
@@ -0,0 +1,18 @@
+
+public class p7 {
+	public static void main(String[]args) {
+		int x=5;
+		int y=10;
+		int z;
+		
+		System.out.println("before swapping = "+x +" and " +y);
+		z=x;
+		x=y;
+		y=z   ;
+		
+		System.out.println("after swapping = "+x +" and "+y);
+	}
+	
+	
+	
+}
