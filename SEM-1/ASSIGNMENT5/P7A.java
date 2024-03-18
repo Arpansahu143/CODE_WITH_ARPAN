58f8f74a741a473a7a3563824ac17921fa667f87
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:58:10 +0530

    Create P7A.java

---
+++ SEM-1/ASSIGNMENT5/P7A.java
@@ -0,0 +1,14 @@
+public class P7A {
+    public static void main(String[] args) {
+        int rows=5;
+        for(int i=1;i<=rows;i++){
+            for(int j=1;j<=i;j++){
+System.out.print((char)(64+j)+" ");
+            }
+            System.out.println();
+        }
+            }
+
+            
+        }
+    
