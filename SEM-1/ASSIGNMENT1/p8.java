12c9e4e366a73dd8a6a149da6a294e2754bd148c
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:54:03 +0530

    Create p8.java

---
+++ SEM-1/ASSIGNMENT1/p8.java
@@ -0,0 +1,21 @@
+
+public class p8 {
+
+	public static void main(String[] args) {
+		int x=5, y=4;
+		System.out.println("before swapping the values are:"+x+","+y);
+		x=x*y;
+		y=x/y;
+		x=x/y;                                                                     
+		
+		
+		System.out.println("after swapping the values are:"+x+","+y);
+				
+		
+				
+				
+			
+
+	}
+
+}
