c8a1af65b96e5955cbe242d5db0b38a5bd8ef32f
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 23:48:49 +0530

    Update Q09.java

---
+++ SEM-2(DSA)/ASSIGNMNET-1/Q09.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Q09 {
+    public static double sumMajorDiagonal(double[][] m) {
+        double sum = 0;
+        for (int i = 0; i < m.length; i++) {
+            sum += m[i][i];
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int row = 4, col = 4;
+        double[][] arr = new double[row][col];
+
+        System.out.print("Enter arr 4-by-4 matrix row by row: ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                arr[i][j] = sc.nextDouble();
+            }
+        }
+
+        double sum = sumMajorDiagonal(arr);
+        System.out.println("Sum of the elements in the major diagonal is " + sum);
+        sc.close();
+    }
+}
