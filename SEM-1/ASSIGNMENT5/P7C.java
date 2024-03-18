2af6e2f7ada4a7e92ee9828ce3163d51e9bae834
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:58:16 +0530

    Create P7C.java

---
+++ SEM-1/ASSIGNMENT5/P7C.java
@@ -0,0 +1,16 @@
+public class P7C {
+    public static void main(String[] args) {
+        int row = 5;
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= row-i; j++) {
+                System.out.print("  ");
+            }
+
+            for (int k = 1; k <= i; k++) {
+                System.out.print(i + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
