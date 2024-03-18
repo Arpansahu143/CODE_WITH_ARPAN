c9fa9ba7b68f200323216a7302e943a4bba6b8df
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 23:42:28 +0530

    Create Q05.java

---
+++ SEM-2(DSA)/ASSIGNMNET-1/Q05.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Q05 {
+    public static int sumOfDigits(int n) {
+        while (n >= 10) {
+            int sum = 0;
+            while (n > 0) {
+                sum += n % 10;
+                n /= 10;
+            }
+            n = sum;
+        }
+        return n;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int res = sumOfDigits(n);
+        System.out.println("Sum of digits of " + n + " until the number is single digit is " + res);
+        sc.close();
+    }
+}
