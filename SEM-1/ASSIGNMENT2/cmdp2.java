3fdf99f80106bd83f8de1434677bd5ab369e8f78
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:54:10 +0530

    Create cmdp2.java

---
+++ SEM-1/ASSIGNMENT2/cmdp2.java
@@ -0,0 +1,14 @@
+
+public class cmdp2 {
+
+	public static void main(String[] args) {
+		System.out.println(args[0]);
+		System.out.println(args[1]);
+		int a=Integer.parseInt(args[0]);
+		int b=Integer.parseInt(args[1]);
+		System.out.println(a+" to the power of "+b+" is: "+(int)Math.pow(a, b));
+		
+
+	}
+
+}
