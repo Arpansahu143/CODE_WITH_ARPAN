6998a29aa334e9c9262056406e5bac6f6094bd09
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 22:57:42 +0530

    Create HA3.java

---
+++ SEM-1/ASSIGNMENT5/HA3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class HA3 {
+
+	public static void main(String[] args) {
+		double sum=0.0d;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter the value of x: ");
+		int x=sc.nextInt();
+		System.out.println("Enter the number of terms: ");
+		int n=sc.nextInt();
+		int p=1;
+		double r=Math.toRadians(x);
+		for(int i=1;i<=n;i++) {
+			int f=1;
+			for(int j=p;j>=1;j--) {
+				f*=j;
+			}
+			
+			double power=Math.pow(r, p);
+			if(i%2==0)
+				sum=sum+(power)/f;
+			else
+				sum=sum-(power)/f;
+			p+=2;
+		}
+		System.out.println("sin("+x+")="+sum);
+	}
+
+}
