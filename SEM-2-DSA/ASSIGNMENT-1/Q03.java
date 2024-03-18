a9d53d1074a480d35538cbef0d76d0fa894f4dab
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-03-18 23:14:50 +0530

    Create Q03.java

---
+++ SEM-2-DSA/ASSIGNMENT-1/Q03.java
@@ -0,0 +1,25 @@
+
+import java.util.Scanner;
+
+public class Q03 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+
+        int sum = 0, mul = 1, temp = n;
+        while (temp != 0) {
+            int digit = temp % 10;
+            sum += digit;
+            mul *= digit;
+            temp /= 10;
+        }
+
+       if(sum==mul){
+        System.out.println("spy number.");
+       }
+       else
+           System.out.println("not spy no.");
+    }
+}
