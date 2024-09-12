a67b8066eeff15558da923ec5959ab403433e20c
Arpansahu143 <150130780+Arpansahu143@users.noreply.github.com>
2024-09-12 23:49:47 +0530

    Create HA8.java

---
+++ SEM-2(DSA)/ASSIGNMENT-3/HA8.java
@@ -0,0 +1,22 @@
+class HA8
+{
+public static void towerOfHanoi(int n, char from_rod, char to_rod, char
+helper_rod)
+{
+if (n == 1)
+{
+System.out.println("Take disk 1 from rod " + from_rod + " to rod " +
+to_rod);
+return;
+}
+towerOfHanoi(n-1, from_rod, helper_rod, to_rod);
+System.out.println("Take disk " + n + " from rod " + from_rod + " to rod " +
+to_rod);
+towerOfHanoi(n-1, helper_rod, to_rod, from_rod);
+}
+public static void main(String[] args)
+{
+int n = 4;
+towerOfHanoi(n,'A','C', 'B');
+}
+}
